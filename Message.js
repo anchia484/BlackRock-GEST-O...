fb0c70af12d8c890d58d30ec0a774bec53ce6c53
anchia484 <anchiasuperior@gmail.com>
2026-04-21 13:38:21 +0200

    Add files via upload

--- Message.js
+++ Message.js
@@ -4,18 +4,8 @@ const messageSchema = new mongoose.Schema({
     usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     remetente: { type: String, enum: ['usuario', 'admin'], required: true },
     texto: { type: String, required: true },
-    lida: { type: Boolean, default: false }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Message', messageSchema, 'mensagens_suporte');
-const mongoose = require('mongoose');
-
-const messageSchema = new mongoose.Schema({
-    usuarioId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    remetente: { type: String, enum: ['usuario', 'admin'], required: true },
-    texto: { type: String, required: true },
     lida: { type: Boolean, default: false },
-    isApagada: { type: Boolean, default: false } // <--- NOVO: Controla se foi anulada
+    isApagada: { type: Boolean, default: false }
 }, { timestamps: true });
 
 module.exports = mongoose.model('Message', messageSchema, 'mensagens_suporte');
