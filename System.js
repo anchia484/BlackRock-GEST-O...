88fa98015d9976b120102f7e4a2ea528353e887e
anchia484 <anchiasuperior@gmail.com>
2026-05-05 12:30:00 +0200

    Add files via upload

--- System.js
+++ System.js
@@ -1,21 +1,6 @@
 const mongoose = require('mongoose');
 
 const systemSchema = new mongoose.Schema({
-    // Financeiro
-    saqueAbre: String, saqueFecha: String, saqueLimite: Number, saqueAtivo: Boolean, saqueTaxa: Number,
-    // Operadoras
-    mpesaNum: String, mpesaNome: String, emolaNum: String, emolaNome: String, depositoMsg: String,
-    // Suporte
-    whatsappLink: String, telegramLink: String, avisoGlobal: String, modoManutencao: Boolean,
-    // Textos Institucionais
-    sobreNos: String, regrasPlataforma: String, faq: String, termosCondicoes: String,
-    // Tarefas
-    tarefasStd: Number, tarefasPre: Number, tarefasVip: Number, tempoTarefa: Number
-}, { timestamps: true });
-
-const mongoose = require('mongoose');
-
-const systemSchema = new mongoose.Schema({
     saqueAbre: String, saqueFecha: String, saqueLimite: Number, saqueAtivo: Boolean, saqueTaxa: Number,
     bonusRede: Number, // <-- ESTA É A MÁGICA NOVA QUE ADICIONAMOS
     mpesaNum: String, mpesaNome: String, emolaNum: String, emolaNome: String, depositoMsg: String,
